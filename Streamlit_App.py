f9dd868b62c27b683740067acaac726ef8d0a70c
Hamza Sassi <109857024+Hamzasassi4@users.noreply.github.com>
2022-07-24 18:02:52 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -18,9 +18,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# New  section to  sisplay Fuityvice API response
-streamlit.header('FruityVice Advice')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+# New  section to  sisplay Fuityvice API response
+streamlit.header('FruityVice Advice')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
