f6bf4d137b429d16c60043e0be57d75218d87881
Hamza Sassi <109857024+Hamzasassi4@users.noreply.github.com>
2022-07-25 16:44:15 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -48,7 +48,7 @@ try:
     
     back_from_function = get_fruityvice_data(fruit_choice)
     # Print the  output  as a frame (table)
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
     #streamlit.text(fruityvice_response.json()) # Write data to the screen
 except URLError as e :
   streamlit.error()
